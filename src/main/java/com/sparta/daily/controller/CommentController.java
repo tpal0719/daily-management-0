c4a851455eeb5022db14614b0cc0de5c20ff365c
varring <tpal071997@gmail.com>
2024-05-28 17:23:22 +0900

    feat - 댓글등록(숙련주차-2단계)

--- src/main/java/com/sparta/daily/controller/CommentController.java
+++ src/main/java/com/sparta/daily/controller/CommentController.java
@@ -1,9 +1,28 @@
 package com.sparta.daily.controller;
 
+import com.sparta.daily.dto.CommentRequestDto;
+import com.sparta.daily.dto.CommentResponseDto;
+import com.sparta.daily.service.CommentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    //댓글 등록
+    @PostMapping("/comment")
+    public CommentResponseDto writeComment(@RequestBody CommentRequestDto requestDto) {
+        return commentService.writeComment(requestDto);
+    }
+
+
 }
--- src/main/java/com/sparta/daily/service/CommentService.java
+++ src/main/java/com/sparta/daily/service/CommentService.java
@@ -1,4 +1,51 @@
 package com.sparta.daily.service;
 
+import com.sparta.daily.dto.CommentRequestDto;
+import com.sparta.daily.dto.CommentResponseDto;
+import com.sparta.daily.entity.Comment;
+import com.sparta.daily.entity.Daily;
+import com.sparta.daily.repository.CommentRepository;
+import com.sparta.daily.repository.DailyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class CommentService {
+
+    @Autowired
+    DailyRepository dailyRepository;
+    @Autowired
+    CommentRepository commentRepository;
+
+    //댓글작성
+    public CommentResponseDto writeComment(CommentRequestDto requestDto) {
+        Comment comment = new Comment(requestDto);
+
+        //선택한 일정의 ID를 입력 받지 않은 경우
+        if(requestDto.getDailyId().describeConstable().isEmpty()){
+
+            throw new IllegalArgumentException("일정이 선택되지 않았습니다.");
+        }
+        // 일정이 DB에 저장되지 않은 경우
+        Daily daily = dailyRepository.findDailyById(requestDto.getDailyId()).orElseThrow(
+
+                () -> new NullPointerException("해당하는 일정이 없습니다.")
+        );
+        //댓글 내용이 비어 있는 경우
+        if (comment.getContents().isBlank()
+                || comment.getContents().isEmpty()) {
+            throw new IllegalArgumentException("댓글 내용이 없습니다.");
+        }
+
+        //save
+        CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
+        Comment savedComment = commentRepository.save(comment);
+        daily.addcommentList(comment);
+        dailyRepository.save(daily);
+
+
+        return commentResponseDto;
+    }
 }
